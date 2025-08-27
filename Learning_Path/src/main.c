007caeb2b1cfdff23e0896004eaa84af7938e8e8
NepherDios <rui.moreira.programmer@gmail.com>
2025-08-27 12:16:30 +0100

    Created new Learning Path folder with main c file

---
+++ Learning_Path/src/main.c
@@ -0,0 +1,15 @@
+/*
+ * main.c
+ *
+ *  Created on: 27/08/2025
+ *      Author: nepher
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	char name[] = "Grim";
+	printf("Hello, %s!", name);
+	return 0;
+}
+
